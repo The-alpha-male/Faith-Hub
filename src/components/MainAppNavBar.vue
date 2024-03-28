5ca3dec8443e11316e72c2d661ebc4e3042354a2
“willy <[email protected]>
2024-03-28 19:35:00 +0300

    updated the navbar and the home page and added images and the logo

--- src/components/MainAppNavBar.vue
+++ src/components/MainAppNavBar.vue
@@ -1,18 +1,53 @@
 <template>
-  <v-app-bar :elevation="2">
-    <template v-slot:prepend>
+  <v-system-bar height="50" color="#77D7D3">
+    <v-container height="50" class="d-flex">
+      <div class="d-flex p-auto">
+        <v-text class="px-4 text-subtitle-1 text-#126360">
+          <v-icon class="pa-2" size="small"> mdi-phone </v-icon>
+          +2547256798
+        </v-text>
+        <p>
+          <v-icon class="px-2" size="small" color="#126360"> mdi-email </v-icon>
+          munenronny@gmail.com
+        </p>
+      </div>
+      <v-spacer></v-spacer>
+      <div>
+        <v-icon size="x-large" class="px-6" color="#126360"> mdi-instagram </v-icon>
+        <v-icon size="x-large" class="px-6" color="#126360"> mdi-facebook </v-icon>
+        <v-icon size="x-large" class="px-6" color="#126360"> mdi-twitter </v-icon>
+        <v-icon size="x-large" class="px-6" color="#126360"> mdi-youtube </v-icon>
+      </div>
+    </v-container>
+  </v-system-bar>
+  <v-app-bar :elevation="0" app height="64">
+    <!-- <template v-slot:prepend>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
     </template>
 
-    <v-app-bar-title>Application Bar</v-app-bar-title>
-    <v-spacer> </v-spacer>
-
-    <v-btn to="/" variant="outlined" size="large" rounded> home </v-btn>
+    <v-app-bar-title>Application Bar</v-app-bar-title> -->
+    <v-img contain src="/src/assets/faith.png" max-height="125" class="ml-16" tile> </v-img>
     <v-spacer></v-spacer>
-    <v-btn to="/about" variant="outlined" size="large" rounded> about us </v-btn>
-    <v-btn to="/about" variant="outlined" size="large" rounded> Ourservices </v-btn>
-    <v-btn to="/about" variant="outlined" size="large" rounded> contact </v-btn>
-    <v-btn to="/about" variant="outlined" size="large" rounded> Blogs </v-btn>
+    <v-row no-gutters class="pa-14">
+      <v-col cols="auto" class="">
+        <v-btn to="/" variant="text"> home </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn to="/about" variant="text"> about us </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn to="" variant="text"> Ourservices </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn to="" variant="text"> contact </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn to="" variant="text"> Blogs </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn color="#FF6D6D" rounded="xl" size="large" bloc to="" variant="flat"> donate </v-btn>
+      </v-col>
+    </v-row>
   </v-app-bar>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>This is my owsome home</div>
+  <div class="container-fluid">
+    <div class="hero-section d-flex justify-center align-center">
+      <v-container class="d-flex">
+        <v-card flat color="transparent" class="mx-auto ml-0" max-width="344">
+          <v-card-text>
+            <div>Word of the Day</div>
+            <p class="text-h4 text--primary">be•nev•o•lent</p>
+            <p>adjective</p>
+            <div class="text--primary">
+              well meaning and kindly.<br />
+              "a benevolent smile"
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="deep-purple-accent-4" variant="text"> Learn More </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,3 +32,16 @@ export default defineComponent({
   components: {}
 })
 </script>
+<style scoped>
+.container-fluid {
+  background-image: url('/src/assets/homeimage.jpeg');
+  height: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+.hero-section {
+  background: linear-gradient(103.5deg, rgba(19, 35, 45, 0.69) 3.46%, rgba(29, 39, 45, 0) 51.8%);
+  height: 970px;
+}
+</style>
