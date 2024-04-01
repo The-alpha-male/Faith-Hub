05a2840cc75a2ef12d2f800ac538fb22dacdb35f
“willy <[email protected]>
2024-04-01 13:56:39 +0300

    updated the homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,6 +39,41 @@
       </v-container>
     </div>
   </div>
+  <div>
+    <v-container>
+      <v-row no-gutters>
+        <v-col cols="4" class="no-space" v-for="carditem in inforCards" :key="carditem.color">
+          <div
+            :style="{
+              width: '401px',
+              height: '351px',
+              backgroundColor: carditem.color,
+              marginTop: '-100px',
+              Padding: '67px, 40px, 68px, 40px'
+            }"
+            class="d-flex justify-center align-center"
+          >
+            <v-card color="transparent" flat height="216px" width="321px">
+              <v-card-title class="mt-2 card-title">
+                {{ carditem.title }}
+              </v-card-title>
+              <v-card-text>
+                {{ carditem.content }}
+              </v-card-text>
+              <v-card-actions v-if="carditem.title === 'Education'">
+                <v-btn class="" style="background-color: #ffffff" rounded size="large">
+                  Discover
+                </v-btn>
+              </v-card-actions>
+              <v-card-actions v-else>
+                <a href="" style="color: #ffffff">JOIN ORGANIZATION</a>
+              </v-card-actions>
+            </v-card>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -49,20 +84,41 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'HomeView',
 
-  components: {}
+  components: {},
+
+  data: () => ({
+    inforCards: [
+      {
+        color: '#77D7D3',
+        title: 'Education',
+        content:
+          'Below Poverty Line is enchmark used by the government of India economic disadvantage.'
+      },
+      {
+        color: '#2E4049',
+        title: 'Become a Volunteer',
+        content: 'Below Poverty Line is enchmark used by the government'
+      },
+      {
+        color: 'white',
+        title: '',
+        content: ''
+      }
+    ]
+  })
 })
 </script>
 <style scoped>
 .container-fluid {
   background-image: url('/src/assets/homeimage.jpeg');
-  height: 100%;
+  height: 920px;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
 }
 .hero-section {
   background: linear-gradient(103.5deg, rgba(19, 35, 45, 0.69) 3.46%, rgba(29, 39, 45, 0) 51.8%);
-  height: 970px;
+  height: 920px;
 }
 .welcome-text {
   font-family: 'Shippori Mincho';
@@ -75,5 +131,16 @@ export default defineComponent({
   font-size: 18px;
   line-height: 21.11px;
   font-weight: 500;
+}
+.no-space {
+  padding: 0 !important;
+  margin: 0 !important;
+}
+.card-title {
+  font-family: 'Shippori Mincho';
+  font-size: 35px;
+  font-weight: 800;
+  line-height: 37.9px;
+  text-align: left;
 }
 </style>
